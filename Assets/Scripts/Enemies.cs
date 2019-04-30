23069b5c1665b48df9e4c07fbd07fdb4e1fc6d9e
Martin James McBride II <mj3mcbr@siena.edu>
2019-04-30 18:20:57 -0400

    Add files via upload

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3f;
+    Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+         moveSpeed = -moveSpeed;
+    }
+}
---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    [SerializeField] int sceneNumber = 1;
+    [SerializeField] float LevelLoadDelay = 2f;
+    void OnTriggerEnter2D()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
